0c43bdaaad970609e158e556830dc95f59aaeb6e
Colin Carter <colin_carter@fmail.co.uk>
2017-08-02 20:18:34 +0100

    Ensuring there is a 'none' state field

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ class BooksApp extends React.Component {
         thumbnail:
           "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
       }
-    ]
+    ],
+    none: []
   };
 
   moveBookToShelf = (book, from, to) => {
