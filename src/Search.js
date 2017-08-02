4a7d7835857565d353c54b8a05c9728dc81a6e0f
Colin Carter <colin_carter@fmail.co.uk>
2017-08-02 18:48:53 +0100

    Ensuring we don't display anything when there's nothing in search term
    Also focus search box when component mounted

--- src/Search.js
+++ src/Search.js
@@ -8,13 +8,18 @@ class Search extends React.Component {
   constructor() {
     super();
     this.state = { books: [], query: "" };
-    this.requestID = null;
   }
+
+  componentDidMount = () => {
+    this.searchInput.focus();
+  };
 
   queryAPI = query => {
     BooksAPI.search(query).then(books => {
       if (books && !books.error) {
-        this.setState({ books, query });
+        if (this.searchInput.value !== "") {
+          this.setState({ books, query });
+        }
       }
     });
   };
@@ -52,11 +57,7 @@ class Search extends React.Component {
   };
 
   render() {
-    let { books, query } = this.state;
-
-    if (query === "") {
-      books = [];
-    }
+    let { books } = this.state;
 
     return (
       <div className="search-books">
@@ -68,18 +69,11 @@ class Search extends React.Component {
             Close
           </a>
           <div className="search-books-input-wrapper">
-            {/*
-    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-    You can find these search terms here:
-    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-    you don't find a specific author or title. Every search is limited by search terms.
-  */}
             <input
               type="text"
               placeholder="Search by title or author"
               onChange={this.onChange}
+              ref={input => (this.searchInput = input)}
             />
           </div>
         </div>
