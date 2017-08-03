015ac55008d7fcc2c61cddb84f69840cdb7cf47f
Colin Carter <colin_carter@fmail.co.uk>
2017-08-03 15:34:10 +0100

    Allowing for a book id

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 export const rawBookToBook = rawBook => {
   return {
+    id: rawBook.id,
     title: rawBook.title,
     authors: rawBook.authors || "",
     thumbnail: rawBook.imageLinks ? rawBook.imageLinks.thumbnail : ""
